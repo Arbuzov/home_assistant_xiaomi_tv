75540a37a7bc88d2d8d787fee66f8b646e2ad4cc
Serge Arbuzov <info@whitediver.com>
2025-05-17 21:22:08 +0000

    Enhance MyProxyView with logging for proxied URL retrieval and error handling
    Signed-off-by: Serge Arbuzov <info@whitediver.com>

--- custom_components/xiaomi_tv/proxy.py
+++ custom_components/xiaomi_tv/proxy.py
@@ -1,4 +1,7 @@
+import logging
 from .hass_web_proxy_lib import ProxyView, ProxiedURL
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class MyProxyView(ProxyView):
@@ -8,8 +11,13 @@ class MyProxyView(ProxyView):
     name = "api:xiaomi_tv:proxy"
 
     def _get_proxied_url(self, request):
-        """Возвращает объект ProxiedURL с параметрами проксирования."""
+        """Get the proxied URL."""
         target_url = request.query.get("url")
         if not target_url:
-            raise ValueError("Параметр 'url' не указан")
-        return ProxiedURL(url=target_url)
+            raise ValueError("The 'url' was not set")
+        _LOGGER.error("Proxied URL: %s", target_url)
+        try:
+            return ProxiedURL(url=target_url)
+        except Exception as err:
+            _LOGGER.error("Error creating ProxiedURL: %s %s", target_url, err)
+            raise
